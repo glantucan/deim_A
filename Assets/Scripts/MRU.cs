219b9e7bb994ee8a40a997424036fb249c8af46d
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-10-05 15:05:56 +0200

    first commit. MRU and MRU_rigidbody

---
+++ Assets/Scripts/MRU.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MRU : MonoBehaviour {
+
+	public float speed;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xInput = Input.GetAxisRaw("Horizontal");
+		float zInput = Input.GetAxisRaw("Vertical");
+		this.direction = xInput * Vector3.right + zInput * Vector3.forward;
+		Vector3 velocity = this.speed * this.direction.normalized;
+		transform.position = transform.position + velocity * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/MRU_rigidbody.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MRU_rigidbody : MonoBehaviour {
+
+	private Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+		rb.velocity = 5F * Vector3.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
