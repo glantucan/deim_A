c2e74fd7bf0ccb7363325b4aa14cf46080406a86
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-12-01 12:34:54 +0100

    corregido examen 3

---
+++ RandomBunchOfFlies/Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	private float v;
+	private Vector3 direction;
+
+	// Use this for initialization
+	private void Start () {
+		v = 4F;
+		direction = new Vector3( Random.Range(-1F, 1F), Random.Range(-1F, 1F), Random.Range(-1F, 1F));
+		//		Random.onUnitSphere;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		Vector3 displacement = v*direction.normalized*Time.deltaTime;
+		this.transform.position += displacement;
+	}
+
+	public void changeDirection() {
+		direction = Random.onUnitSphere;
+
+	}
+}
---
+++ RandomBunchOfFlies/Assets/Scripts/SpheresCreator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpheresCreator : MonoBehaviour {
+
+	[SerializeField] private GameObject prefab;
+	private GameObject[] spheres;
+	private float timer;
+
+	private void Start () {
+		this.spheres = new GameObject[20];
+		int counter = 0;
+		while (counter < 20) {
+			this.spheres[counter] = Object.Instantiate(prefab);
+			this.spheres[counter].transform.position = new Vector3(
+				Random.Range(-5F, 5F),
+				Random.Range(-5F, 5F),
+				Random.Range(-5F, 5F)
+			);
+			counter += 1;
+		}
+		timer = 2F;
+	}
+
+	private void Update () {
+		timer -= Time.deltaTime;
+		if ( timer < 0F){
+			int counter = 0;
+			while (counter < 20) {
+				Mover mover = this.spheres[counter].GetComponent<Mover>();
+				mover.changeDirection();
+				counter += 1;
+			} 
+			timer = 2F;
+		}
+	}
+}
