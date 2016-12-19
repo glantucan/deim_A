f2b9d3aca84f0c8f2ed38a48b16e614eaaf66c25
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-12-19 12:55:54 +0100

    Corregido ejercicio4 de actividad 4

---
+++ ejerciciciosComunicacionClases/ejercicio04/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMovement : MonoBehaviour {
+	private GameObject target;
+	private Vector3 finalPosition;
+	private bool isMoving;
+	[SerializeField] private float v;
+	private Vector3 direction;
+
+
+	// Use this for initialization
+	private void Awake () {
+		this.isMoving = false;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (this.isMoving) {
+			Vector3 displacement = v * direction * Time.deltaTime; 
+			Vector3 distance = finalPosition - this.transform.position;
+
+			if (distance.magnitude > displacement.magnitude) {
+				this.transform.position += displacement;
+			} else  {
+				this.transform.position = finalPosition;
+				isMoving = false;
+				CubeMovement targetMovement = this.target.GetComponent<CubeMovement>();
+				targetMovement.StartMovement();
+			}
+		}
+	}
+
+	public void SetTarget(GameObject theTargetCube) {
+		this.target = theTargetCube;
+		direction = (theTargetCube.transform.position - this.transform.position).normalized;
+		this.finalPosition = theTargetCube.transform.position - 1F * direction;
+	}
+
+	public void StartMovement() {
+		this.isMoving = true;
+	}
+}
---
+++ ejerciciciosComunicacionClases/ejercicio04/Assets/Scripts/Ejercicio04.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ejercicio04 : MonoBehaviour {
+	[SerializeField] private GameObject cubePrefab;
+
+	private GameObject[] cubes;
+
+	private void Start () {
+		cubes = new GameObject[10];
+		int counter = 0;
+
+		while (counter < 10) {
+			cubes[counter] = Object.Instantiate(cubePrefab);
+			cubes[counter].transform.position = counter * 3F * Vector3.right;  
+			counter += 1;
+		}
+
+		counter = 0;
+
+		while (counter < 9) {
+			CubeMovement movement = cubes[counter].GetComponent<CubeMovement>();
+			movement.SetTarget( cubes[counter + 1] );
+			counter += 1;
+		}
+
+		CubeMovement firstCubeMovement = cubes[0].GetComponent<CubeMovement>();
+		firstCubeMovement.StartMovement();
+
+	}
+
+}
